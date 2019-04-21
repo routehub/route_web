1dd8bb627671f43370e930f03e956f3af5cb8a92
ctyo <ctyo@live.jp>
2019-04-21 22:52:55 +0900

    release用のビルド設定を追加

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -33,7 +33,7 @@ export class ListPage implements OnInit {
     this.search(this.query, this.page);
   }
 
-  public wordChanged(event) {
+  public wordChanged() {
     this.page = 0;
     this.query = this.searchText;
     this.items = [];
