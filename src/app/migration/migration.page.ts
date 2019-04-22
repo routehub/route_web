e31b8b4d55585780173bd26b9976318a4d4d4fbd
ctyo <ctyo@live.jp>
2019-04-23 08:05:13 +0900

    取り込み画面のキャプチャを変更

--- src/app/migration/migration.page.ts
+++ src/app/migration/migration.page.ts
@@ -21,19 +21,24 @@ export class MigrationPage implements OnInit {
     //    if (window.clipboardData && window.clipboardData.getData) { // IE pollyfillされるかな...
     //      var pastedText = window.clipboardData.getData('Text');
     //    } else 
-    var importlist = [];
-    if (event.clipboardData && event.clipboardData.getData) {
-      let pastedText: string = event.clipboardData.getData('text/html');
-      let m = pastedText.match(/<tr id="(.+?)"/g);
-      if (m.length <= 0) {
-        return;
-      }
-
-      for (let i = 0; i < m.length; i++) {
-        let id_m = m[i].match(/"(.+?)"/);
-        importlist.push(id_m[1]);
-      }
+    if (!event.clipboardData || !event.clipboardData.getData) {
+      return;
     }
+
+    var importlist = [];
+
+    let pastedText: string = event.clipboardData.getData('text/html');
+    let m = pastedText.match(/<tr id="(.+?)"/g);
+    if (!m || m.length <= 0) {
+      console.log('pasted, but clipboard empty url.');
+      return;
+    }
+
+    for (let i = 0; i < m.length; i++) {
+      let id_m = m[i].match(/"(.+?)"/);
+      importlist.push(id_m[1]);
+    }
+    // TODO: ★をつけたルートは別ロジックで取る（ユーザーに選ばせてあげたほうが良さそう
 
     console.dir(importlist);
 
