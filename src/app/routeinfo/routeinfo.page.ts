6e00634b8128075d8264f253035ad990222963f2
ctyo <ctyo@live.jp>
2019-04-23 00:22:42 +0900

    閉じるボタン追加

--- src/app/routeinfo/routeinfo.page.ts
+++ src/app/routeinfo/routeinfo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-routeinfo',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouteinfoPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController,
+    private navParams: NavParams
+  ) { }
 
   ngOnInit() {
   }
-
+  async closeModal() {
+    await this.modalController.dismiss();
+  }
 }
