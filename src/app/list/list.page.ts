4983429fb4fad1516d2316223c8b6900aa8af65d
ctyo <ctyo@live.jp>
2019-04-26 11:09:19 +0900

    ヤフーのサムネ画像の利用

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IonInfiniteScroll, NavController/*, NavParams */ } from '@ionic/angular';
+import { lineString } from '@turf/helpers';
+import { splitAtColon } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-list',
@@ -11,24 +13,23 @@ export class ListPage implements OnInit {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   @ViewChild('searchbar') searchbar_elem: ElementRef;
 
-  private selectedItem: any;
   private search_url = 'https://dev-api.routelabo.com/route/1.0.0/search';
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ id: string, title: string; author: string; icon: string }> = [];
+  private staticmap_url = 'https://map.yahooapis.jp/map/V1/static';
+  public items: Array<{
+    id: string,
+    title: string;
+    author: string;
+    icon: string;
+    thumburl: string;
+  }> = [];
   private page = 0;
   private query = '';
   searchText = '';
+  private thumbappid = "dj00aiZpPXFPNk1BUG4xYkJvYSZzPWNvbnN1bWVyc2VjcmV0Jng9N2U-";
+  loadingAnimation = "../../assets/loadingAnimation.gif";
+
+
+
   constructor(private http: HttpClient, public navCtrl: NavController/*, public navParams: NavParams*/) {
     this.search(this.query, this.page);
   }
@@ -65,7 +66,8 @@ export class ListPage implements OnInit {
             id: r.id,
             title: r.title,
             author: r.author,
-            icon: 'beer'
+            icon: 'beer',
+            thumburl: this.getThumbUrl(r.summary),
           });
 
           this.infiniteScroll.complete();
@@ -74,12 +76,15 @@ export class ListPage implements OnInit {
         const response: any = res;
         return response;
       });
-
-    'http://localhost:8080/route/1.0.0/route?id=4355bc819e024a613f92f6c13ccd8bd9'
   }
 
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+  getThumbUrl(summary) {
+    let line = summary.slice(11, -1).split(',').map(pos => {
+      let p = pos.split(' ');
+      return p[1] + ',' + p[0];
+    }).join(',');
+    return this.staticmap_url + '?appid=' + this.thumbappid
+      + '&autoscale=on&scalebar=off&width=300&height=200&l=' + '255,52,106,90,3,'
+      + line;
+  }
 }
