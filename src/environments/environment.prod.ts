0aec7f20bdfb995bf40bb49a42aed166fb925ad6
shinji ichien <shinji11088011@gmail.com>
2019-04-27 13:33:32 +0900

    firebaseの設定をenvironment.prod.tsに追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // TODO 本番運用用に変更する
+  firebase: {
+    apiKey: 'AIzaSyAUNac1J0FeWxE82q3kwuHfkds9aPz-VE8',
+    authDomain: 'route-lab.firebaseapp.com',
+    databaseURL: 'https://route-lab.firebaseio.com',
+    projectId: 'route-lab',
+    storageBucket: 'route-lab.appspot.com',
+    messagingSenderId: '409666348022'
+  }
 };
