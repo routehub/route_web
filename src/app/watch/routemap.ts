8675dfafa9f1b6988795b027fb819fe0e4441cd2
ctyo <ctyo@live.jp>
2019-05-02 15:31:15 +0900

    リファクタリグ

--- src/app/watch/routemap.ts
+++ src/app/watch/routemap.ts
@@ -2,6 +2,33 @@ import { Events } from '@ionic/angular';
 import * as L from 'leaflet';
 import * as Elevation from 'leaflet.elevation/src/L.Control.Elevation.js';
 
+/***
+ * ルートModel
+ * いろんなところで使いまわしたい
+ */
+export class Route {
+    id: string;
+    title: string;
+    body: string;
+    tag: string;
+    author: string;
+    summary: string;
+    thumburl: string;
+    is_gps: boolean;
+    created_at: string;
+    private staticmap_url = 'https://map.yahooapis.jp/map/V1/static';
+    private thumbappid = "dj00aiZpPXFPNk1BUG4xYkJvYSZzPWNvbnN1bWVyc2VjcmV0Jng9N2U-";
+    private getThumbUrl(summary) {
+        let line = summary.slice(11, -1).split(',').map(pos => {
+            let p = pos.split(' ');
+            return p[1] + ',' + p[0];
+        }).join(',');
+        return this.staticmap_url + '?appid=' + this.thumbappid
+            + '&autoscale=on&scalebar=off&width=450&height=300&l=' + '0,0,255,105,4,' // rgb, a, weight
+            + line;
+    }
+}
+
 export class Routemap {
 
     gpsIcon = new L.icon({
@@ -86,7 +113,5 @@ export class Routemap {
 
         return { map: map, elevation: elevation };
     }
-
-
 }
 
