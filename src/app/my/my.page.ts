2d3940dc27ae259387a05e55e7196f8ed08091dc
ctyo <ctyo@live.jp>
2019-05-02 19:21:21 +0900

    マイページにログイン情報取得ロジック追加

--- src/app/my/my.page.ts
+++ src/app/my/my.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { NavController } from '@ionic/angular';
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
 
 @Component({
   selector: 'app-my',
@@ -7,17 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyPage implements OnInit {
 
-  constructor() { }
+  uid;
+  photoURL;
+  displayName;
+  body = "最近ロードのってない最近ロードのってない最近ロードのってない最近ロードのってない最近ロードのってない最近ロードのってない最近ロードのってない最近ロードのってない最近ロードのってない"
+
+  constructor(
+    private navCtrl: NavController,
+    private storage: Storage,
+  ) { }
 
   ngOnInit() {
     // ログイン確認
-
-    // 非ログインの場合は /loginへ遷移
+    var that = this;
+    // TODO : なんかココらへん処理をまとめたほうが良さそう, JSONで格納したほうがよさそう
+    this.storage.get('user.uid').then((uid) => {
+      // 非ログインの場合は /loginへ遷移
+      if (ɵPLATFORM_WORKER_UI_ID == null) {
+        this.navCtrl.navigateForward('/login');
+      }
+      that.uid = uid;
+    }).catch(e => {
+      // 非ログインの場合は /loginへ遷移
+      this.navCtrl.navigateForward('/login');
+    });
+    this.storage.get('user.displayName').then((displayName) => {
+      this.displayName = displayName;
+    });
+    this.storage.get('user.photoURL').then((photoURL) => {
+      that.photoURL = photoURL;
+    });
 
   }
 
   ionViewWillEnter() {
     // searchでfirebase_tokenで検索
+    // 遷移きちんと確認した暗号つくる
 
   }
 
