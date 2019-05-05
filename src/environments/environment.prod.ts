1bfe30e5f8eb47247281d4c6050df8edc62c415e
ctyo <ctyo@live.jp>
2019-05-05 17:49:10 +0900

    fix environment setting bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,14 @@ export const environment = {
     projectId: 'route-lab-63f4a',
     storageBucket: 'route-lab-63f4a.appspot.com',
     messagingSenderId: '1000468098052'
+  },
+
+    api: {
+    'host': 'https://dev-api.routelabo.com/route/1.0.0',
+    'search_path': '/search',
+    'my_path': '/my',
+    'staticmap_url': 'https://map.yahooapis.jp/map/V1/static',
+    'thumbappid': "dj00aiZpPXFPNk1BUG4xYkJvYSZzPWNvbnN1bWVyc2VjcmV0Jng9N2U-",
+
   }
 };
