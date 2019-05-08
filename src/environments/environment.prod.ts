f051e5e015ae7934582aa9e16842193524403ca8
ctyo <ctyo@live.jp>
2019-05-09 06:39:04 +0900

    fix prod setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,10 +10,11 @@ export const environment = {
     messagingSenderId: '1000468098052'
   },
 
-    api: {
+  api: {
     'host': 'https://dev-api.routelabo.com/route/1.0.0',
     'search_path': '/search',
     'my_path': '/my',
+    'like_path': '/like',
     'staticmap_url': 'https://map.yahooapis.jp/map/V1/static',
     'thumbappid': "dj00aiZpPXFPNk1BUG4xYkJvYSZzPWNvbnN1bWVyc2VjcmV0Jng9N2U-",
 
