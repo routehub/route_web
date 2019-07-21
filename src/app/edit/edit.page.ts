c0a9781dbde1d131f7948c5c8e280c0be5f7f77a
shinji ichien <shinji11088011@gmail.com>
2019-07-21 23:41:42 +0900

    Androidアプリのデバッグビルドまで

--- src/app/edit/edit.page.ts
+++ src/app/edit/edit.page.ts
@@ -87,8 +87,7 @@ export class EditPage implements OnInit {
       // TODO : SP用の動作をまた実装する(hammer panが便利)
       this.presentToast('ルート編集モードに変更');
 
-      that.hammer.on('tap', function(ev)
-      {
+      that.hammer.on('tap', function (ev) {
         let header_height = 64;
         if (that.platform.is('mobile')) {
           header_height = 10;
@@ -97,36 +96,31 @@ export class EditPage implements OnInit {
         let _point = L.point(ev.center.x, ev.center.y - header_height);
         let latlng = that.map.containerPointToLatLng(_point);
 
-        let overlap_marker = that.find_nearest_marker_from_latlng( latlng, 16.0 );
-        if ( overlap_marker != null )
-        {
+        let overlap_marker = that.find_nearest_marker_from_latlng(latlng, 16.0);
+        if (overlap_marker != null) {
           return;
         }
 
         let marker = L.marker(latlng, { icon: that.routemap.editIcon, draggable: true });
-        let marker_data = new MarkerData( that, marker );
-        
-        marker.on('dragend', function(e)
-        {
+        let marker_data = new MarkerData(that, marker);
+
+        marker.on('dragend', function (e) {
           console.log("dragend");
-          marker_data.move_marker( e.target._latlng );
+          marker_data.move_marker(e.target._latlng);
         });
 
-        marker.on('click', function(e)
-        {
+        marker.on('click', function (e) {
           console.log("click");
           marker_data.remove_marker();
         });
 
         // 経由点追加テスト
-        let overlap_route = that.find_nearest_route_point_from_latlng( latlng, 16.0 );
-        if ( overlap_route == null )
-        {
-          that.push_marker( marker_data );
+        let overlap_route = that.find_nearest_route_point_from_latlng(latlng, 16.0);
+        if (overlap_route == null) {
+          that.push_marker(marker_data);
         }
-        else
-        {
-          that.insert_marker( marker_data, overlap_route.next_data );
+        else {
+          that.insert_marker(marker_data, overlap_route.next_data);
         }
       });
 
@@ -139,19 +133,17 @@ export class EditPage implements OnInit {
 
   }
 
-  find_nearest_marker_from_latlng( a_latlng, a_distance: number )
-  {
+  find_nearest_marker_from_latlng(a_latlng, a_distance: number) {
     let that = this;
-    let latlng_point = that.map.latLngToContainerPoint( a_latlng );
+    let latlng_point = that.map.latLngToContainerPoint(a_latlng);
     let dist2 = a_distance * a_distance;
 
     let res = null;
     let min_dist = 0.0;
 
-    for ( let i = 0 ; i < that.editMarkers.length ; ++i )
-    {
+    for (let i = 0; i < that.editMarkers.length; ++i) {
       let mark = that.editMarkers[i];
-      let point = that.map.latLngToContainerPoint( mark.marker._latlng );
+      let point = that.map.latLngToContainerPoint(mark.marker._latlng);
 
       let delta_x = point.x - latlng_point.x;
       let delta_y = point.y - latlng_point.y;
@@ -159,13 +151,11 @@ export class EditPage implements OnInit {
       let dist = delta_x * delta_x + delta_y * delta_y;
       // console.log("dist: ", Math.sqrt( dist ) );
 
-      if ( dist > dist2 )
-      {
+      if (dist > dist2) {
         continue;
       }
 
-      if ( ( res != null ) && ( dist >= min_dist ) )
-      {
+      if ((res != null) && (dist >= min_dist)) {
         continue;
       }
 
@@ -176,74 +166,64 @@ export class EditPage implements OnInit {
     return res;
   }
 
-  find_nearest_route_point_from_latlng( a_latlng, a_distance: number )
-  {
+  find_nearest_route_point_from_latlng(a_latlng, a_distance: number) {
     let that = this;
-    let latlng_point = that.map.latLngToContainerPoint( a_latlng );
+    let latlng_point = that.map.latLngToContainerPoint(a_latlng);
     let dist2 = a_distance * a_distance;
 
     let res = null;
     let min_dist = 0.0;
 
-    for ( let i = 0 ; i < that.editMarkers.length ; ++i )
-    {
+    for (let i = 0; i < that.editMarkers.length; ++i) {
       let mark = that.editMarkers[i];
 
-      if ( mark.bounds == null )
-      {
+      if (mark.bounds == null) {
         continue;
       }
 
-      if ( !mark.bounds.contains( a_latlng ) )
-      {
+      if (!mark.bounds.contains(a_latlng)) {
         continue;
       }
 
       let route = mark.route;
 
-      for ( let j = 0 ; j < route.length ; ++j )
-      {
+      for (let j = 0; j < route.length; ++j) {
         // let point = that.map.latLngToContainerPoint( route[j] );
-        let latlng = L.latLng( route[j][1], route[j][0] );
-        let point = that.map.latLngToContainerPoint( latlng );
+        let latlng = L.latLng(route[j][1], route[j][0]);
+        let point = that.map.latLngToContainerPoint(latlng);
 
         let delta_x = point.x - latlng_point.x;
         let delta_y = point.y - latlng_point.y;
-  
+
         let dist = delta_x * delta_x + delta_y * delta_y;
         // console.log("dist: ", Math.sqrt( dist ) );
-  
-        if ( dist > dist2 )
-        {
+
+        if (dist > dist2) {
           continue;
         }
-  
-        if ( ( res != null ) && ( dist >= min_dist ) )
-        {
+
+        if ((res != null) && (dist >= min_dist)) {
           continue;
         }
-  
+
         res = mark;
         min_dist = dist;
-        }
+      }
     }
 
     return res;
-}
+  }
 
-  remove_geojson()
-  {
+  remove_geojson() {
     let that = this;
 
-    if ( that.geojson != null )
-    {
+    if (that.geojson != null) {
       that.geojson.removeFrom(that.map);
       that.geojson = null;
     }
   }
 
-  refresh_geojson()
-  {
+  refresh_geojson() {
     let that = this;
 
     that.remove_geojson();
@@ -262,7 +242,7 @@ export class EditPage implements OnInit {
       this.map.removeLayer(this.hotlineLayer);
       this.hotlineLayer = false;
       this.isSlopeMode = false;
-    }    
+    }
     that.geojson = L.geoJson(that.route_geojson, {
       "color": "#0000ff",
       "width": 6,
@@ -282,13 +262,11 @@ export class EditPage implements OnInit {
     // });
   }
 
-  refresh_route()
-  {
+  refresh_route() {
     let that = this;
 
     that.line = [];
-    for ( let i = 0 ; i < that.editMarkers.length - 1 ; ++i )
-    {
+    for (let i = 0; i < that.editMarkers.length - 1; ++i) {
       // console.log(that.editMarkers[i].route.length);
       that.line = that.line.concat(that.editMarkers[i].route);
     }
@@ -296,24 +274,21 @@ export class EditPage implements OnInit {
     that.refresh_geojson();
   }
 
-  push_marker( a_markar_data: MarkerData )
-  {
+  push_marker(a_markar_data: MarkerData) {
     let that = this;
 
-    a_markar_data.marker.addTo( that.map );
+    a_markar_data.marker.addTo(that.map);
 
-    that.editMarkers.push( a_markar_data );
+    that.editMarkers.push(a_markar_data);
 
-    if ( that.editMarkers.length >= 2 )
-    {
+    if (that.editMarkers.length >= 2) {
       let start_data = that.editMarkers[that.editMarkers.length - 2];
       let goal_data = that.editMarkers[that.editMarkers.length - 1];
 
-      start_data.set_next( goal_data );
+      start_data.set_next(goal_data);
 
-      start_data.routing().then( () =>
-      {
-        that.line = that.line.concat(start_data.route);        
+      start_data.routing().then(() => {
+        that.line = that.line.concat(start_data.route);
         console.log(that.line.length);
 
         that.refresh_geojson();
@@ -321,64 +296,55 @@ export class EditPage implements OnInit {
     }
   }
 
-  insert_marker( a_markar_data: MarkerData, a_position: MarkerData )
-  {
+  insert_marker(a_markar_data: MarkerData, a_position: MarkerData) {
     let that = this;
 
-    a_markar_data.marker.addTo( that.map );
+    a_markar_data.marker.addTo(that.map);
 
-    for ( let i = 0 ; i < that.editMarkers.length ; ++i )
-    {
-      if ( that.editMarkers[i] !== a_position )
-      {
+    for (let i = 0; i < that.editMarkers.length; ++i) {
+      if (that.editMarkers[i] !== a_position) {
         continue;
       }
 
       let prev = that.editMarkers[i].prev_data;
       let next = that.editMarkers[i];
 
-      let last = that.editMarkers.splice( i, that.editMarkers.length - i);
-      that.editMarkers.push( a_markar_data );
-      that.editMarkers = that.editMarkers.concat( last );
+      let last = that.editMarkers.splice(i, that.editMarkers.length - i);
+      that.editMarkers.push(a_markar_data);
+      that.editMarkers = that.editMarkers.concat(last);
 
-      a_markar_data.set_next( next );
-      a_markar_data.set_prev( prev );
+      a_markar_data.set_next(next);
+      a_markar_data.set_prev(prev);
 
       a_markar_data.refresh_marker();
       break;
     }
   }
 
-  remove_markar( a_markar_data: MarkerData )
-  {
+  remove_markar(a_markar_data: MarkerData) {
     let that = this;
 
-    a_markar_data.marker.removeFrom( that.map );
+    a_markar_data.marker.removeFrom(that.map);
 
-    for ( let n = 0 ; n < that.editMarkers.length ; ++n )
-    {
-      if ( that.editMarkers[n] !== a_markar_data )
-      {
+    for (let n = 0; n < that.editMarkers.length; ++n) {
+      if (that.editMarkers[n] !== a_markar_data) {
         continue;
       }
 
-      that.editMarkers.splice( n, 1 );
+      that.editMarkers.splice(n, 1);
       break;
     }
   }
 
 
-  async routing(start, goal)
-  {
+  async routing(start, goal) {
     // let url = 'http://localhost:8080/route/1.0.0/routing?start=' + start + '&' + 'goal=' + goal;
     let url = 'https://routing.routehub.app/route/1.0.0/routing?start=' + start + '&' + 'goal=' + goal;
-    return await this.http.get(url).toPromise().then( (res: any) =>
-    {
+    return await this.http.get(url).toPromise().then((res: any) => {
       let ret = [];
 
-      res.forEach( (p: any) =>
-      {
-        ret.push( [ p[1], p[0], p[2], ] );
+      res.forEach((p: any) => {
+        ret.push([p[1], p[0], p[2],]);
       });
 
       return ret;
@@ -405,8 +371,7 @@ export class EditPage implements OnInit {
     }
   }
 
-  async presentToast(message)
-  {
+  async presentToast(message) {
     const toast = await this.toastController.create({
       message: message,
       duration: 2000,
@@ -420,146 +385,119 @@ export class EditPage implements OnInit {
 
 
 
-class MarkerData
-{
+class MarkerData {
   marker: L.marker;
   next_data: MarkerData;
   prev_data: MarkerData;
   route = [];
   bounds: L.latLngBounds;
 
-  constructor( private edit_page: EditPage, a_marker: L.markar )
-  {
+  constructor(private edit_page: EditPage, a_marker: L.markar) {
     this.marker = a_marker;
   }
 
-  set_next( a_next_data: MarkerData )
-  {
+  set_next(a_next_data: MarkerData) {
     let that = this;
 
     that.next_data = a_next_data;
-    if ( a_next_data != null )
-    {
+    if (a_next_data != null) {
       a_next_data.prev_data = that;
     }
   }
 
-  set_prev( a_prev_data: MarkerData )
-  {
+  set_prev(a_prev_data: MarkerData) {
     let that = this;
 
     that.prev_data = a_prev_data;
-    if ( a_prev_data != null )
-    {
+    if (a_prev_data != null) {
       a_prev_data.next_data = that;
     }
   }
 
-  async routing()
-  {
+  async routing() {
     let that = this;
 
-    if ( that.next_data != null )
-    {
+    if (that.next_data != null) {
       let start = that.marker._latlng.lat + ',' + that.marker._latlng.lng;
       let goal = that.next_data.marker._latlng.lat + ',' + that.next_data.marker._latlng.lng;
 
-      await that.edit_page.routing(start, goal).then( (_route: any) =>
-      {
+      await that.edit_page.routing(start, goal).then((_route: any) => {
         that.route = _route;
 
-        if ( _route.length > 0 )
-        {
-          let latlng_min = L.latLng( _route[0][1], _route[0][0] );
-          let latlng_max = L.latLng( _route[0][1], _route[0][0] );
+        if (_route.length > 0) {
+          let latlng_min = L.latLng(_route[0][1], _route[0][0]);
+          let latlng_max = L.latLng(_route[0][1], _route[0][0]);
 
-          for ( let i = 1 ; i < _route.length ; ++i )
-          {
-            latlng_max.lat = Math.max( latlng_max.lat, _route[i][1] );
-            latlng_max.lng = Math.max( latlng_max.lng, _route[i][0] );
+          for (let i = 1; i < _route.length; ++i) {
+            latlng_max.lat = Math.max(latlng_max.lat, _route[i][1]);
+            latlng_max.lng = Math.max(latlng_max.lng, _route[i][0]);
 
-            latlng_min.lat = Math.min( latlng_min.lat, _route[i][1] );
-            latlng_min.lng = Math.min( latlng_min.lng, _route[i][0] );
+            latlng_min.lat = Math.min(latlng_min.lat, _route[i][1]);
+            latlng_min.lng = Math.min(latlng_min.lng, _route[i][0]);
           }
 
-          that.bounds = L.latLngBounds( latlng_min, latlng_max );
+          that.bounds = L.latLngBounds(latlng_min, latlng_max);
         }
-        else
-        {
+        else {
           that.bounds = null;
         }
       });
     }
-    else
-    {
+    else {
       that.route = [];
       that.bounds = null;
     }
   }
 
-  refresh_marker()
-  {
+  refresh_marker() {
     let that = this;
 
-    that.routing().then( () =>
-    {
-      if ( that.prev_data != null )
-      {
-        that.prev_data.routing().then( () =>
-        {
+    that.routing().then(() => {
+      if (that.prev_data != null) {
+        that.prev_data.routing().then(() => {
           that.edit_page.refresh_route();
         });
       }
-      else
-      {
+      else {
         that.edit_page.refresh_route();
       }
     });
   }
 
-  move_marker( a_latlng: L.latLng )
-  {
+  move_marker(a_latlng: L.latLng) {
     let that = this;
 
-    that.marker.setLatLng( a_latlng );
+    that.marker.setLatLng(a_latlng);
 
     that.refresh_marker();
   }
 
-  remove_marker()
-  {
+  remove_marker() {
     let that = this;
 
     let prev = that.prev_data;
     let next = that.next_data;
 
-    if ( prev != null )
-    {
-      prev.set_next( next );
+    if (prev != null) {
+      prev.set_next(next);
     }
-    if ( next != null )
-    {
-      next.set_prev( prev );
+    if (next != null) {
+      next.set_prev(prev);
     }
 
-    that.edit_page.remove_markar( that );
+    that.edit_page.remove_markar(that);
 
-    if ( prev != null )
-    {
-      prev.routing().then ( () =>
-      {
+    if (prev != null) {
+      prev.routing().then(() => {
         that.edit_page.refresh_route();
       });
     }
-    else if ( next != null )
-    {
-      next.routing().then ( () =>
-      {
+    else if (next != null) {
+      next.routing().then(() => {
         that.edit_page.refresh_route();
       });
     }
-    else
-    {
+    else {
       that.edit_page.refresh_route();
     }
   }
